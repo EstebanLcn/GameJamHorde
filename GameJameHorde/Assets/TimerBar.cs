a9763a89712a8921a4a4a72db5ca26997e16b83c
dmonfumat - lahorde <d.monfumat@lahorde.school>
2022-03-06 10:04:50 +0100

    working timer

--- GameJameHorde/Assets/TimerBar.cs
+++ GameJameHorde/Assets/TimerBar.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerBar : MonoBehaviour
 {
     public Slider slider;
     int maxStamina = 10;
+    Scene m_scene;
+    string sceneName;
+
 
     private void Start()
     {
         slider.maxValue = maxStamina;
         slider.value = 0;
+        m_scene = SceneManager.GetActiveScene();
+        sceneName = m_scene.name;
     }
 
     private void Update()
     {
         slider.value += 1 * Time.deltaTime;
+
+        if (slider.value == slider.maxValue)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
